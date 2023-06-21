95880bfba232009ff6ac01dc5aac76d1ae998622
Muslimah Sarumi <sarumiatinuke2020@gmail.com>
2023-06-21 14:33:41 +0100

    new files added

--- src/data.js
+++ src/data.js
@@ -23,4 +23,16 @@ export const data = [
       type: 'image',
       path: "/file-server/image.png"
     },
+    {
+      id: 5,
+      name: 'Image File',
+      type: 'image',
+      path: "/file-server/image2.png"
+    },
+    {
+      id: 6,
+      name: 'Image File',
+      type: 'image',
+      path: "/file-server/image3.png"
+    }
   ]
